da9e36140c34193af8882399ff4d47306838eec6
BATHTUBGERALT1 <43067557+BATHTUBGERALT1@users.noreply.github.com>
2018-09-14 15:10:48 +0100

    decimals removed from time remaining

--- Assets/Scripts/ButtonMasher.cs
+++ Assets/Scripts/ButtonMasher.cs
@@ -7,17 +7,31 @@ public class ButtonMasher : MonoBehaviour {
     // data for our class
     public AudioSource clickSound; // sound that will play when we click the button 
     public TextMesh scoreText; // display text for the player score 
+    public TextMesh timerText; // display text for the time remaining 
+    public float gameLength; // how many seconds will the game last 
+
 
     private int score = 0; // this is a numerical data value of our score 
+    private float timeRemaining = 0; // numerical time remaining for the game
+
 
 	// Use this for initialization
 	void Start () {
         Debug.Log("Start method called");
+
+        timeRemaining = gameLength; 
 	} // End of Start()
 	
 	// Update is called once per frame
 	void Update () {
+
         Debug.Log("Method Called");
+
+        // update the numerical time remaining 
+        timeRemaining = timeRemaining - Time.deltaTime;
+
+        // update the visual time remaining 
+        timerText.text = (Mathf.CeilToInt (timeRemaining)).ToString(); 
 		
 	} // End of Update()
 
